a660a215f6c8395dba9a19280a2f1a1b18a0ddfd
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-11 11:59:38 -0400

    Added reporting to hamsters.rb"

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -6,6 +6,15 @@ def set_default_age(age)
   end
 end
 
+def report_age(age)
+  if age.nil?
+    puts "What an incredible find!  You have in your possession one of the fabled Timeless Hamsters.  Keep it well and you will be blessed with good fortune!"
+  else
+    puts "Hamster is estimated to be #{age} months old."
+  end
+end
+
+
 def set_default_candidate_status(candidate)
   if candidate == 'N' 
     candidate = false          
@@ -13,6 +22,15 @@ def set_default_candidate_status(candidate)
     candidate = true  # because no hamster is born bad!
   end
 end
+
+def good_candidate?(candidate)
+  if candidate == "Y" || candidate == true
+    true
+  else
+    false
+  end
+end
+
 
 puts "What is the hamster's name?"
 name = gets.chomp
@@ -31,3 +49,15 @@ candidate = set_default_candidate_status(candidate)
 puts "how old would you say this hamster in months?"
 age = gets.chomp
 age = set_default_age(age)
+
+puts "Hamster's name:         #{name}"
+puts "Prefered volume level:  #{volume}"
+puts "Hamster's fur color:    #{fur}"
+puts "Candidacy:"
+if good_candidate?(candidate) 
+  puts "   No hamster is born bad and this one seems like a fine pet for you!"
+else
+  puts "   Unfortunately this hamster has picked up some very bad habits, so adopt at your own risk!"
+end
+puts "Estimated age: "
+report_age(age)
