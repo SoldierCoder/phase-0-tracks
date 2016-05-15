39d0260634f29be676ef612b8ff3be531c81177f
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-15 11:42:43 -0400

    add method calls to produce the desired results

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,42 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".swapcase!
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# ("zom".insert 1, "o")
+# ("zom".insert 2, "o")
 # => “zoom”
 
-# "enhance".<???>
+# "enhance".rjust(11).ljust(15)
+# "enhance".ljust(11).rjust(15)
+#  enhance".rjust(11)+ "    "
+#  enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# "the usual".concat(" suspects")
+# "the usual" + (" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+# " suspects".rjust(18, "the usual")
+# " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".reverse.chop.reverse
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# "z".sum #this is probably THE most useless thing ever!
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
