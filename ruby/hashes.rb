b516bfb89fc47e62811c5abbacba5c80cdeba38f
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-15 22:59:24 -0400

    add last_chance_edit

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,10 +1,13 @@
 def report(client)
-  puts "client: #{client[:first_name]} #{client[:last_name]}"
-  puts "age: #{client[:age]}"
-  reside_str = "This client does"
-  does_or_not =  (client[:in_US]) ? ' ' : ' does not '
-  reside_str = reside_str + does_or_not + 'reside in the United States'
-  puts reside_str
+  puts client
+
+  
+  # puts "client: #{client[:first_name]} #{client[:last_name]}"
+  # puts "age: #{client[:age]}"
+  # reside_str = "This client does"
+  # does_or_not =  (client[:in_US]) ? ' ' : ' does not '
+  # reside_str = reside_str + does_or_not + 'reside in the United States'
+  # puts reside_str
 end
 
 
@@ -42,4 +45,39 @@ client = {
 }
 
 report(client)
- 
+
+puts "Last chance!  which item would you like to edit? (first_name, last_name, age, in_US, or done)"
+last_chance_edit = gets.chomp.downcase
+input_buffer = ''
+
+case 
+  when last_chance_edit == "first_name"
+    
+    puts "first_name should be set to: ?"
+    input_buffer = gets.chomp.capitalize!
+    client.store(:first_name, input_buffer)
+
+  when last_chance_edit == "last_name" 
+    
+    puts "last_name should be set to: ?"
+    input_buffer = gets.chomp.capitalize!
+    client.store(:last_name, input_buffer)
+
+  when last_chance_edit == "age"
+
+    puts "age should be set to: ?"
+    input_buffer = gets.chomp.to_i
+    client.store(:age, input_buffer)
+
+  when last_chance_edit == "in_US"
+    
+    puts "does the client live in the US? (Y/n)"
+    input_buffer = gets.chomp.upcase!
+    in_US = ((input_buffer == 'Y') ? true : false)
+    client.store(:in_US, in_US)
+else last_chance_edit == "done"
+
+end
+
+puts client
+
