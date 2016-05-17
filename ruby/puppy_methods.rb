8c069c7ed3b91bb356045744fd8cc22000dd2ca8
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-17 07:59:06 -0400

    added Ninjas, put them thru paces

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -32,14 +32,42 @@ class Puppy
 
 end
 
-annie = Puppy.new
-trisha = Puppy.new
-annie.fetch("the mailman")
-annie.speak(2)
-annie.roll_over
-puts "I am #{annie.dog_years(5)}"
-trisha.at_night(30)
-annie.at_night(80)
+# annie = Puppy.new
+# trisha = Puppy.new
+# annie.fetch("the mailman")
+# annie.speak(2)
+# annie.roll_over
+# puts "I am #{annie.dog_years(5)}"
+# trisha.at_night(30)
+# annie.at_night(80)
+
+class Ninja
+
+  def initialize
+      puts "Ninja initialized"
+  end
+
+  def juggles 
+      puts "​*items flip madly through air in succession*​"
+  end
+
+  def saves_humankind
+      puts "Hurray, Hurray! We are saved!"
+  end
+    
+end
+
+green_ninja = Ninja.new
+
+green_ninja.juggles
+
+army_of_the_green_ninjas = Array.new(50) { Ninja.new }
 
 
+little_ninjas = [] 
+50.times  {little_ninjas << Ninja.new }
 
+little_ninjas.each  do |ninja| 
+  ninja.juggles
+  ninja.saves_humankind
+end
