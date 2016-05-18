578608f4bc5e6fbd3537fa56805732b84dd40517
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-18 16:55:10 -0400

    added driver code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,27 +1,67 @@
-"for encrypt, we will find the length of an accepted string
-   iterate each letter of the string
-       increment each letter by one"
-
-
-
-def encrypt(plaintext)
+# encrypt : we will find the length of an accepted string
+#   iterate each letter of the string
+#       increment each letter by one
+def encrypt(plaintext)   
   l = plaintext.length
-  for i in 0...l do
-    
-    if plaintext[i] != ' '
+  for i in 0...l do 
+    if plaintext[i] == "z"
+      plaintext[i] = "a"
+    elsif (plaintext[i] != ' ')
       letter = plaintext[i]
       letter = letter.next
       plaintext[i] = letter
     end
-
   end
   plaintext
 end
+# given a string to decrypt, we will use an variable with all the letters of the alphabet in order, to find 
+# the index of a letter in the decrpyt string and decrement that index to find the letter that should be 
+#  substituted in the decrypt string
+def decrypt(encipher)
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  l = encipher.length
+  for i in 0...l do 
+    if encipher[i] == "a"
+      encipher[i] = "z"
+    elsif (encipher[i] != " ")
+      letter = encipher[i]
+      letter = alpha.index(letter)
+      letter = letter - 1
+      encipher[i] = alpha[letter]
+    end
+  end
+  encipher
+end
 
-" given a string to decrypt, we will use an variable with all the letters of the alphabet in order, to find 
-//  the index of a letter in the decrpyt string and decrement that index to find the letter that should be 
-//  substituted in the decrypt string"
 
-s = 'ed was here'
-encrypt(s)
-puts(s)
+def test_it
+  puts encrypt("abc")
+  puts(encrypt("zed")) 
+  puts(decrypt("bcd"))
+  puts(decrypt("afe"))
+end
+
+
+def driver
+
+ #test_it
+ #puts decrypt(encrypt("swordfish")) # this works because the return value of encrypt becomes the input for decrypt
+
+  puts "Would you like to encrypt(1) or decrypt(2)?"
+
+  choice = gets.chomp.to_i
+  puts "choice is #{choice}"
+
+  puts "enter your password"
+  password = gets.chomp
+
+  if choice == 1
+    password = encrypt(password)   
+  else choice == 2
+    password = decrypt(password)
+  end
+
+  puts "your password is #{password}" 
+end
+
+driver
