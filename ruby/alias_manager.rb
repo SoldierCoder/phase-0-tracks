6672bfc3ce53f18659b70a91445536c0729c9705
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-18 08:46:25 -0400

    begin psuedocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+given: a spy's real name
+when: we have the spy's first and last name
+then we will swap the first and last name using split on spaces
+
+given swapped names (last and first) 
+when they are elements in an array
+then we will join them back together using join
+
+given one joined string of last and first names separated by a space
+when we want to encode the vowels 
+then we will set up an array: only_vowels
+then we will iterate over the vowels in the names charified array
+  when we encounter a vowel in our names charified array
+  then we will replace it with  the next vowel in the array
+
+
