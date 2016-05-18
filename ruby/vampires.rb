f9b66c58193841e4dc28d01242494e9aad3eb4f2
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-18 10:25:56 -0400

    release 1

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,21 +1,34 @@
 # Vampires by Ed Drain
+def question_response_boolean(question)
+  puts question
+  response = gets.chomp.upcase!
+  boolean_return = ((response == 'Y') ? true : false)
+end
+
+def question_response_integer(question)
+  puts question
+  response = gets.chomp.to_i
+end
 
 def survey
-  puts 'What is your name?'
-  name = gets.chomp.split(' ').map(&:capitalize).join(' ')
-  puts "name is #{name}"
+  # puts 'What is your name?'
+  # name = gets.chomp.split(' ').map(&:capitalize).join(' ')
+  # puts "name is #{name}"
 
-  puts 'How old are you?'
-  age = gets.chomp.to_i
-  puts "age: #{age}"
+  # age = question_response_integer('How old are you?')
+  # puts "age: #{age}"
 
-  puts 'What year were you born?'
-  birth_year = gets.chomp.to_i
-  puts "year of birth: #{birth_year}"
+  # birth_year =  question_response_integer('What year were you born?')
+  # puts "year of birth: #{birth_year}"
 
-  puts 'The cafe here serves a mean garlic bread.  May we offer you some, on the house? (Y/n)'
-  takes_garlic = gets.chomp
-  if takes_garlic.upcase == 'Y'
+  takes_garlic = question_response_boolean('The cafe here serves a mean garlic bread.  May we offer you some, on the house? (Y/n)')
+  puts takes_garlic ? 'Participant does take garlic!' : 'Participant does not take garlic'
+
+  takes_insurance = question_response_boolean('Would you like health insurance? (Y/n')
+  puts takes_insurance ? 'Participant takes insurance' : 'Participant mumbled something about the Affordable Care Act...'
+
+  
+  if takes_garlic
     puts "this guy is not a Vampire, as he loves the Garlic Bread!"
   end
 
