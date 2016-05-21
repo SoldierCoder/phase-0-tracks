4eb2483510a08655857328bbdac3aa95f4d47947
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-21 08:57:02 -0400

    release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,12 +17,48 @@ class Santa
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie}!"
   end
+  
+  def celebrate_birthday
+    @age = @age + 1
+  end
+  
+  def get_mad_at(reindeer_name)
+    idx = @reindeer_ranking.index(reindeer_name)
+    @reindeer_ranking.delete_at(idx)
+    @reindeer_ranking.push(reindeer_name) 
+  end
+
+  def gender=(to_gender)
+    @gender = to_gender
+  end
+
+  def gender
+    @gender
+  end
+
+  def age
+    @age
+  end
+  
+  def ethnicity
+    @ethnicity
+  end
+  def report
+    s = "Santas age is #{@age}.  "
+    celebrate_birthday
+    s = s + "But wait, Santa JUST had a birthday!  Santa age now is: #{@age}\nSanta is peaceful and happy...\n"
     
+
+    puts s
+  end
 end
 
 
 santas = []
 santas << Santa.new("female", "Ukrainian")
+santas.last.report
+santas.last.gender = "N/A"
+puts "Santa's gender is #{santas.last.gender}"
 santas << Santa.new("female", "Latino")
 santas << Santa.new("bigender", "white")
 santas << Santa.new("male", "Japanese")
