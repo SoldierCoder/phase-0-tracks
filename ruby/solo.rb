22f6c0541168dcc5c2786f3d687fc659f7c6f0af
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-21 13:15:49 -0400

    designed class as pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,21 @@
+# class PillBottle
+#
+# Attributes
+#
+#   warnings will be an array of strings representing reasons not to take whatever pills are in this bottle
+#   
+#   directions will list directions for how often to take the pills in the bottle
+#
+#   pills will be an integer count of how many pills remain in the bottle
+#
+# Instance methods
+#
+#   open allows someone to call the methods dispense_pill or fill
+#
+#   fill(x)  when the PillBottle has been opened the manufacturer of the pill may fill it with a given number of pills 
+#   
+#   close  -- closes the pill bottle
+#
+#   dispense(x) when the PillBottle has been opened, calling this method allowes x number of pills to be dispensed 
+
+
