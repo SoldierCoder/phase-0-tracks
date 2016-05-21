52a3e299b22cb878c4a82c51dcec4ca9c8d21a28
Ed Drain <geekprogrammer.ed@gmail.com>
2016-05-21 07:49:34 -0400

    Gave Santa gender, ethnicity, age and reindeer_ranking attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,13 @@
 class Santa
-  def initialize
+  def initialize(gender, ethnicity)
+    
     puts "initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+
+    @reindeer_ranking = ['Rudolph', 'Dasher', 'Dancer',
+      'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen']
+    @age = 0
   end
 
   def speaks
@@ -10,8 +17,15 @@ class Santa
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie}!"
   end
+    
 end
 
-ed = Santa.new
-ed.speaks
-ed.eat_milk_and_cookies "peanut butter"
+
+santas = []
+santas << Santa.new("female", "Ukrainian")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "Latino")
