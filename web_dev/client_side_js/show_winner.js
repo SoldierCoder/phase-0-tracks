78187b9eb59702f9f913a0658b9f8a5567e19799
Ed Drain <geekprogrammer.ed@gmail.com>
2016-06-11 23:01:12 -0700

    add all the things

--- web_dev/client_side_js/show_winner.js
+++ web_dev/client_side_js/show_winner.js
@@ -2,3 +2,13 @@ var a_winner = document.getElementsByClassName("winner");
 for(var i=0; i < a_winner.length; i++) {
   a_winner[i].style.border = "2px dashed";
 }
+document.getElementById("timeshifter").addEventListener("click", function(){
+  document.getElementById("last_x").innerHTML = "&nbsp;O&nbsp;";
+  for(var i=0; i < a_winner.length; i++) {
+    a_winner[i].style.border = "";
+  };
+  var comments = document.createElement("P id='comments'");
+  document.getElementById("timeshifter").appendChild(comments);
+  document.getElementById("comments").innerHTML = "Ha Ha";
+
+});
